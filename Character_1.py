b932c40db55676dcf301b71f3b8e78b871025f77
jun23314 <dlwnsfml@naver.com>
2023-12-04 23:11:12 +0900

    .

--- Character_1.py
+++ Character_1.py
@@ -2,10 +2,11 @@ import numpy as np
 from PIL import Image, ImageDraw
 from BackGround import BackGround
 from PIL import Image
+
 class Character_1:
     def __init__(self, position, background):
         background = background.shape.crop((position[0], position[1], position[0]+70, position[1]+70))
-        self.shape = Image.open("character_1.PNG").convert('RGBA')
+        self.shape = Image.open("character_1.png").convert('RGBA')
         self.shape = Image.alpha_composite(background, self.shape)
         self.life = 5
         self.safe = np.array([position[0] , position[1] , position[0] + 50, position[1] + 50])
