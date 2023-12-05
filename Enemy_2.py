74ee7e0c90c011a7bacf35edf7518c727dbe6fc5
jun23314 <dlwnsfml@naver.com>
2023-12-05 17:12:42 +0900

    .

--- Enemy_2.py
+++ Enemy_2.py
@@ -3,10 +3,10 @@ import numpy as np
 
 class Enemy_2:
     def __init__(self, position, background):
-        background = background.crop((position[0], position[1], position[0]+30, position[1]+30))
+        background = background.crop((position[0], position[1], position[0]+70, position[1]+70))
         self.shape = Image.open("tree_1.png").convert('RGBA')
         self.shape = Image.alpha_composite(background, self.shape)
-        self.attack = np.array([position[0]+5, position[1]+5, position[0] + 25, position[1] + 25])
+        self.attack = np.array([position[0]+20, position[1]+20, position[0] + 50, position[1] + 50])
         self.touch = 1
         self.state = 'live'
         self.life = 9
