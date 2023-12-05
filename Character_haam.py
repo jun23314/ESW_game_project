9173e98df3818bc5f527a83ae586ad72bbf5f423
jun23314 <dlwnsfml@naver.com>
2023-12-05 23:09:34 +0900

    .

--- Character_haam.py
+++ Character_haam.py
@@ -12,7 +12,7 @@ class Character_haam:
         self.center = np.array([position[0]+15, position[1]+15])
         
 
-    def collision_check(self, character, enemys, character_):
+    def collision_check(self, character, enemys):
         for enemy in enemys[0]:
             collision = self.overlap(character.attack, enemy.attack)
             if collision:
