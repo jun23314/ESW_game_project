66d69a6439987bc4f09e55b7496f6be25b75968a
jun23314 <dlwnsfml@naver.com>
2023-12-05 15:23:58 +0900

    .

--- Enemy_1.py
+++ Enemy_1.py
@@ -3,10 +3,10 @@ import numpy as np
 
 class Enemy_1:
     def __init__(self, position, background):
-        background = background.crop((position[0], position[1], position[0]+70, position[1]+70))
+        background = background.crop((position[0], position[1], position[0]+30, position[1]+30))
         self.shape = Image.open("plant.png").convert('RGBA')
         self.shape = Image.alpha_composite(background, self.shape)
-        self.attack = np.array([position[0]+20, position[1]+20, position[0] + 50, position[1] + 50])
+        self.attack = np.array([position[0]+5, position[1]+5, position[0] + 25, position[1] + 25])
         self.touch = 1
         self.state = 'live'
         self.life = 15
@@ -14,4 +14,4 @@ class Enemy_1:
 
     def death(self, background):
         self.state = 'death'
-        self.shape_ = background.crop((self.position[0], self.position[1], self.position[0]+24, self.position[1]+24))
+        self.shape_ = background.crop((self.position[0], self.position[1], self.position[0]+30, self.position[1]+30))
