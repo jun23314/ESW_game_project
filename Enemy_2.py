fda813d1ee56ed079d84e6d4b9bb30ca9985b600
jun23314 <dlwnsfml@naver.com>
2023-12-05 17:09:35 +0900

    .

--- Enemy_2.py
+++ Enemy_2.py
@@ -1,15 +1,15 @@
 from PIL import Image
 import numpy as np
 
-class Enemy_1:
+class Enemy_2:
     def __init__(self, position, background):
         background = background.crop((position[0], position[1], position[0]+30, position[1]+30))
-        self.shape = Image.open("plant.png").convert('RGBA')
+        self.shape = Image.ope("tree_1.png").convert('RGBA')
         self.shape = Image.alpha_composite(background, self.shape)
         self.attack = np.array([position[0]+5, position[1]+5, position[0] + 25, position[1] + 25])
         self.touch = 1
         self.state = 'live'
-        self.life = 3
+        self.life = 9
         self.position = position
 
     def death(self, background):
