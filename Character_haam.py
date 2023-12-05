ee26f5fe78426dc83e951c85af254f0ee8eec2be
jun23314 <dlwnsfml@naver.com>
2023-12-05 21:42:04 +0900

    .

--- Character_haam.py
+++ Character_haam.py
@@ -8,12 +8,12 @@ class Character_haam:
         self.shape = Image.alpha_composite(background, self.shape)
         self.life = 5
         self.safe = np.array([position[0] , position[1] , position[0] + 30, position[1] + 30])
-        self.state = None
         self.position = position
         self.center = np.array([position[0]+15, position[1]+15])
+        
 
     def collision_check(self, character, enemys, character_):
-        for enemy in enemys[::-1]:
+        for enemy in enemys[0]:
             
             collision = self.overlap(character.safe, enemy.attack)
 
@@ -23,4 +23,4 @@ class Character_haam:
                     break
 
     def overlap(self, enemys_position, my_position):
-        return (my_position[2] >enemys_position[0] > my_position[0] or my_position[2] >= enemys_position[2] >= my_position[0]) and (my_position[1] <= enemys_position[1] <= my_position[3] or my_position[1] <= enemys_position[3] <= my_position[3])
+        return (my_position[2] > enemys_position[0] > my_position[0] or my_position[2] >= enemys_position[2] >= my_position[0]) and (my_position[1] <= enemys_position[1] <= my_position[3] or my_position[1] <= enemys_position[3] <= my_position[3])
