18214c9db48987280b1a6858f02a2c0a21d26f98
jun23314 <dlwnsfml@naver.com>
2023-12-07 03:37:28 +0900

    .

--- Enemy_boss.py
+++ Enemy_boss.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Enemy_boss:
     def __init__(self, position, background):
-        background = background.crop((position[0], position[1], position[0]+70, position[1]+70))
+        background = background.crop((position[0], position[1], position[0]+150, position[1]+150))
         self.shape = Image.open("enemy_boss.png").convert('RGBA')
         self.shape = Image.alpha_composite(background, self.shape)
         self.attack = np.array([position[0]+20, position[1]+20, position[0] + 50, position[1] + 50])
@@ -14,4 +14,4 @@ class Enemy_boss:
 
     def death(self, background):
         self.state = 'death'
-        self.shape_ = background.crop((self.position[0], self.position[1], self.position[0]+70, self.position[1] + 70))
+        self.shape_ = background.crop((self.position[0], self.position[1], self.position[0]+150, self.position[1] + 150))
