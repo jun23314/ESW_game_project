43f0cf881e966b35fc964ceaddaff9551b8cbe73
jun23314 <dlwnsfml@naver.com>
2023-12-07 04:24:23 +0900

    .

--- Character_hurt.py
+++ Character_hurt.py
@@ -6,5 +6,4 @@ class Character_hurt:
         background = background.shape.crop((position[0], position[1], position[0]+30, position[1]+30))
         self.shape = Image.open("character_hurt.png").convert('RGBA')
         self.shape = Image.alpha_composite(background, self.shape)
-        self.life = 5
         self.position = position
