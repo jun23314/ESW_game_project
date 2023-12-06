cfdb12e2022b0bb5c70895fb2b81ecd05ab39bd9
jun23314 <dlwnsfml@naver.com>
2023-12-06 23:10:01 +0900

    .

--- Character_haam_plant.py
+++ Character_haam_plant.py
@@ -1,7 +1,7 @@
 import numpy as np
 from PIL import Image
 
-class Character_haam:
+class Character_haam_plant:
     def __init__(self, position, background):
         background = background.shape.crop((position[0], position[1], position[0]+30, position[1]+30))
         self.shape = Image.open("character_haam_plant.png").convert('RGBA')
